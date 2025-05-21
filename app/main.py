ff38b719d20d820b209b16afa8367b6b830695a8
Bekuma Negese Gedefa <Bekuma.Negese@coopbankoromiasc.com>
2025-05-21 14:13:15 +0300

    feat: basic Streamlit UI

---
+++ app/main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+st.set_page_config(page_title="Solar Insights Dashboard", layout="wide")
+
+#data = pd.read_csv(r"C:\Users\bekuman\Desktop\moonlight-solar-strategy-analysis\data\togo_clean.csv")
+#st.write(data)
+
+
+st.write("hello world")
+st.title("My title")
+
+st.button("Click me")
+
